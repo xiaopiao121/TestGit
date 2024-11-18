7dc910f582be70fbc6d9ab4bd39c937c656e2005
xiaopiao121 <389142946@qq.com>
2024-11-18 10:20:45 +0800

    lc 在Task.java文件中增加了注释。

--- src/main/java/com/cn/git/Task.java
+++ src/main/java/com/cn/git/Task.java
@@ -2,6 +2,7 @@ package com.cn.git;
 
 public class Task {
    public static void main(String[] args) {
-	System.out.println("task..");
-}
+		System.out.println("task..");
+		System.out.println("这是一个Task文件");
+	}
 }
