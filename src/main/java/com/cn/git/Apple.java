30b353b3df7907db3a6c699266fff71e2f410607
xiaopiao121 <389142946@qq.com>
2024-11-22 11:29:36 +0800

    lc 提交修改。

--- src/main/java/com/cn/git/Apple.java
+++ src/main/java/com/cn/git/Apple.java
@@ -4,5 +4,6 @@ public class Apple {
     public static void main(String[] args) {
 	    System.out.println("飘更新了这个文件");
 		System.out.println("这是一个苹果类");
+		System.out.println("用户李四对当前类进行补充");
 	}
 }
