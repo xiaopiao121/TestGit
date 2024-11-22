7cca34d57aaddc17fedd20bfde27e29b62798032
飘 <33832882+xiaopiao121@users.noreply.github.com>
2024-11-22 11:28:12 +0800

    Update Apple.java

--- src/main/java/com/cn/git/Apple.java
+++ src/main/java/com/cn/git/Apple.java
@@ -3,6 +3,7 @@ package com.cn.git;
 public class Apple {
     public static void main(String[] args) {
 	    System.out.println("飘更新了这个文件");
-		System.out.println("这是一个苹果类");
+	    System.out.println("这是一个苹果类");
+	    System.out.println("用户用户张三修改了当前行");
 	}
 }
